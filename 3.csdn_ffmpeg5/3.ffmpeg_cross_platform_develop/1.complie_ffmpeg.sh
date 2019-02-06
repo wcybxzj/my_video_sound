59f92367ea776f05c1b92c6d58950176c70bb7d8
ybx <87212426@qq.com>
2019-02-07 01:32:32 +0800

    finish lesson4 but demo work fail

--- 3.csdn_ffmpeg5/3.ffmpeg_cross_platform_develop/1.complie_ffmpeg.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-cd /Users/ybx/soft/ffmpeg-3.3
-rm -rf ./output
-./configure --arch=x86_64 --target-os=darwin --disable-yasm --prefix=./output \
-			--disable-ffmpeg --disable-ffplay --disable-doc --disable-ffprobe --disable-bzlib --disable-ffserver
-make -j8 install
